1b1f170863b2e7ca1daf0b314587f748a85190cd
AMITH <AMITHDA1945@GMAIL.COM>
2026-06-17 12:10:40 +0530

    Add GitHub Actions workflow for automatic deployment to GitHub Pages

--- notification-app-fe/vite.config.js
+++ notification-app-fe/vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/16005/',
   plugins: [react()],
   server: {
     port: 3000,
